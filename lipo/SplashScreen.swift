990edb7c2f19b1afc1bc219b281e6448d50e8e61
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2021-08-31 01:56:25 +0900

    アイコンの設定と、起動画面の設定が完了、次回は配列の重複をなんとかしたい。

---
+++ lipo/SplashScreen.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct LaunchScreen: View {
+    @State private var isLoading = true
+
+    var body: some View {
+        if isLoading {
+            ZStack {
+                VStack{
+                Text("Drone Battery Info").font(.title)
+                Image("drone")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    HStack{
+                        Image(systemName: "battery.100")
+                        .resizable()
+                        .frame(width: 50.0, height: 50.0, alignment: .leading)
+                        Image(systemName: "battery.100")
+                        .resizable()
+                        .frame(width: 50.0, height: 50.0, alignment: .leading)
+                        Image(systemName: "battery.100")
+                            .resizable()
+                            .frame(width: 50.0, height: 50.0, alignment: .leading)
+                    }}}
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    withAnimation {
+                        isLoading = false
+                    }
+                }
+            }
+        } else {
+            ContentView()
+        }
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
